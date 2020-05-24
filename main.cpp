6e75f2656ada40a809f453164f111a866548a443
midbel <brun.nicolas21@gmail.com>
2020-05-24 09:23:27 +0200

    set font to app

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFont font;
+    font.setFamily(font.defaultFamily());
+    font.setPointSize(9);
+    a.setFont(font);
+
     NetWindow w;
     w.show();
     return a.exec();
